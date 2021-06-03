16730ecb0f9fd4b4828f49485ba943c9740573fd
Roman-H-Joy <85276039+Roman-H-Joy@users.noreply.github.com>
2021-06-03 12:45:12 +0600

    Update quote.js

--- quote.js
+++ quote.js
@@ -12,22 +12,22 @@ const quotes = [{
     quote: "If life were predictable it would cease to be life, and be without flavor.",
     author: `– Eleanor Roosevelt`
 }, {
-    quote: `"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."`,
+    quote: `If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.`,
     author: `– Oprah Winfrey`
 }, {
-    quote: `"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."`,
+    quote: `If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.`,
     author: `– James Cameron `
 }, {
-    quote: `"Life is what happens when you're busy making other plans."`,
+    quote: `Life is what happens when you're busy making other plans.`,
     author: `– John Lennon`
 }, {
-    quote: `"Spread love everywhere you go. Let no one ever come to you without leaving happier."`,
+    quote: `Spread love everywhere you go. Let no one ever come to you without leaving happier.`,
     author: `– Mother Teresa`
 }, {
-    quote: `"When you reach the end of your rope, tie a knot in it and hang on."`,
+    quote: `When you reach the end of your rope, tie a knot in it and hang on.`,
     author: `-Franklin D. Roosevelt`
 }, {
-    quote: `"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."`,
+    quote: `The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.`,
     author: `- Helen Keller`
 }, ]
 
@@ -49,4 +49,6 @@ btn.addEventListener("click", function() {
 
     //Show name
     author.innerHTML = quotes[random].author;
+    
+    //Thanks For Wath ###
 })
